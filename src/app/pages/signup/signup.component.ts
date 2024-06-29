b655ad8227262006779c911a26111614ad14af27
Samuel Rógenes <rogenescarvalho@hotmail.com>
2024-06-29 11:22:39 -0300

    feat: singup created

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,13 +6,15 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm{
+interface SignupForm{
+  name: FormControl
   email: FormControl
   password: FormControl
+  passwordConfirm: FormControl
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
@@ -22,31 +24,33 @@ interface LoginForm{
   providers: [
     LoginService
   ],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastr: ToastrService
   ) {
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
 
   submit() {
-    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => this.toastr.success("Login feito com sucesso"),
-      error: () => this.toastr.error("Login falhou")
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+      next: () => this.toastr.success("Cadastro feito com sucesso"),
+      error: () => this.toastr.error("Cadastro falhou")
     });
   }
 
   navigate() {
-    this.router.navigate(["signup"]);
+    this.router.navigate(["login"]);
   }
 }
